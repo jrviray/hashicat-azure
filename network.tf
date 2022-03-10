f0447743d0f6adfb724ad6f40209b0717a0f6fda
jrviray <joshuaviray.cs@gmail.com>
2022-03-10 23:59:27 +0000

    updated network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/viray-training/network/azurerm"
   version = "3.5.0"
-  # insert required variables here
+  resource_group_name = azurerm.resource_group_name.myresourcegroup.name
 }
